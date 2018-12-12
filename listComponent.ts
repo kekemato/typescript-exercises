942db80f0db89cee48855bdac0726b38de4fdb7f
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-12-12 13:38:56 +0100

    made few interfaces

--- listComponent.ts
+++ listComponent.ts
@@ -1,7 +1,25 @@
 interface EventsList {
-    events: Array<object>
+    events: Array<Event>
     filterCategory: string
     filterText: string
     numberOfUsers: string
     isFavourite: Function
+}
+
+interface Event {
+    name: string
+    date: Date
+    key: string
+    photo: URL
+    city: string
+    street: string
+}
+
+interface SearchForm {
+    onFilteredTextChangeHandler: Function
+    handleUsersChange: Function
+    handleEventsFilterCategoryChange: Function
+    filterCategory: string
+    filterText: string
+    numberOfUsers: number
 }
