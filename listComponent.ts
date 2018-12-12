335a38a46d6b60b4f8bbc6fda782c071eef8d779
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-12-12 14:16:56 +0100

    finished tasks

--- listComponent.ts
+++ listComponent.ts
@@ -3,23 +3,29 @@ interface EventsList {
     filterCategory: string
     filterText: string
     numberOfUsers: string
-    isFavourite: Function
+    isFavourite(event: Event): void
 }
 
 interface Event {
     name: string
     date: Date
     key: string
-    photo: URL
+    photo: string
     city: string
     street: string
 }
 
 interface SearchForm {
-    onFilteredTextChangeHandler: Function
-    handleUsersChange: Function
-    handleEventsFilterCategoryChange: Function
+    onFilteredTextChangeHandler(p1: SuperEvent) : void
+    handleUsersChange(p1: SuperEvent) : void
+    handleEventsFilterCategoryChange(p1: SuperEvent) : void
     filterCategory: string
     filterText: string
     numberOfUsers: number
+}
+
+interface SuperEvent {
+    target: {
+        value: string
+    }
 }
